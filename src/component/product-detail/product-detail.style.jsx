4a436a5d71596b9a3fb3abb844d41a53381f6266
ReeseSun <98066506+WeiruSun@users.noreply.github.com>
2022-06-27 22:17:10 -0500

    add product detail(in progress)

---
+++ src/component/product-detail/product-detail.style.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ProductDetailContainer = styled.div`
+  display: flex;
+  align-content: center;
+  justify-content: center;
+`;
+
+export const ProductContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
